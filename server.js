820af8eacf1285c6e84b01df0fb747fedcd9e3f7
NarekHakobyanJS <haobyan95narek@gmail.com>
2024-09-09 18:29:02 +0400

    1 end

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs')
 
+
+ 
 const server = http.createServer((req, res) => {
 
     if (req.url === '/' && req.method === "GET") {
@@ -16,7 +18,24 @@ const server = http.createServer((req, res) => {
                 res.end()
             })
     }
+    else if(req.url === '/api/users' && req.method === "GET"){
+        fs.promises.readFile('./db/users.json', 'utf-8')
+            .then((data) => {
+                res.writeHead(200, {statusMessage : "succesful users", 'Content-Type' : 'application/json'})
+                res.write(data)
+                res.end()
+            })
+            .catch((err) => {
+                res.end()
+            })
+    }
+    else {
+        res.end()
+    }
 })
 
 
 server.listen(3003, () => console.log("server is running!!"))
+
+
+
